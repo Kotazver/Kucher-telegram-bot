a685cd11f1b033d2c2269e3d21aaf87f4d721437
Kotazver <maksimtimakov904@gmail.com>
2026-03-14 02:04:35 +0200

    basic utilities to work with json files

--- src/utils.lua
+++ src/utils.lua
@@ -1,14 +1,32 @@
-utils = {}
+UTILS = {}
 
 local json = require("dkjson")
 
-utils.getJsonContent = function (path)
+UTILS.getJsonContent = function (path)
     local content = nil
     local file = io.open(path,"r")
 
     if file then
         local json_string = file:read("a")
         content = json.decode(json_string)
+        file:close()
         return content
+    else
+        return false, "can't open file " .. path
     end
 end
+
+UTILS.createJsonFile = function (path,table)
+    if not table or not path then return false, "invalid arguments" end
+
+    local file = io.open(path,"w+")
+    if file then
+        file:write(json.encode(table,{indent = true}))
+        file:close()
+        return true
+    else
+        return false,"can't write file " .. path
+    end
+end
+
+return UTILS
